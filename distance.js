60a2e970527ee0f574b469fa88ca4cf736274afa
djwzg <46942584+djwzg@users.noreply.github.com>
2019-04-25 15:37:43 -0700

    Update distance.js

--- distance.js
+++ distance.js
@@ -7,19 +7,17 @@ function rad(d){
    
 function getDistance(lat1, lng1, lat2, lng2){
 	if( ( Math.abs( lat1 ) > 90  ) ||(  Math.abs( lat2 ) > 90 ) )
-	  returnfalse;
+	  return false;
    
 	if( ( Math.abs( lng1 ) > 180  ) ||(  Math.abs( lng2 ) > 180 ) )
-	  returnfalse;
+	  return false;
    
-	let varradLat1 = rad(lat1);
-	let varradLat2 = rad(lat2);
-	let vara = radLat1 - radLat2;
-	let varb = rad(lng1) - rad(lng2);
-	let vars = 2 * Math.asin(
-		Math.sqrt(
-		  Math.pow( Math.sin( a/2 ), 2 ) + Math.cos( radLat1 ) * Math.cos( radLat2 ) *
-		  Math.pow( Math.sin( b/2 ), 2 )
+	let radLat1 = rad(lat1);
+	let radLat2 = rad(lat2);
+	let a = radLat1 - radLat2;
+	let b = rad(lng1) - rad(lng2);
+	let s = 2 * Math.asin(
+		Math.sqrt(Math.pow( Math.sin( a/2 ), 2 ) + Math.cos( radLat1 ) * Math.cos( radLat2 ) * Math.pow( Math.sin( b/2 ), 2 )
 		)
 	);
 	s = s * 6378.137 ; 
@@ -31,8 +29,9 @@ function checkDistence(lat, lng){
     let distToSac = getDistance(lat, lng, 38.575764, -121.478851);
     distToSac = 1.60934 * distToSac;
 
+
     if(distToSac >= 150)
-        returnfalse;
+        return false;
    
     return;
 }   
