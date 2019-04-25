cf15eae2edcc38a91f2e67785e0931dc172c3b5f
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2019-04-25 12:40:40 -0700

    added display time

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-import { iconGetter } from "./weather-condition.mjs";
+"use strict";
+
+import { timeGetter } from "./display-time.mjs";
+
+console.log(timeGetter.displayTime("2017-09-23 10:00:00"));
