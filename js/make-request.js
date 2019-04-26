1865245eb2191f1ed423860aac3c6ac92901499a
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2019-04-25 20:25:04 -0700

    merged display time into make-request.js

--- js/make-request.js
+++ js/make-request.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Make requests to retrive information from Open Weather Map
 
 const requestMaker = new (function () {
 	
@@ -54,6 +55,84 @@ const requestMaker = new (function () {
 	});
 	
 })();
+
+// Convert time to am/pm format, given the UTC 24-hour format time on Open Weather Map
+
+const timeGetter = new (function () {
+		
+		function getUTCDateString(openWeatherMapDate) {
+			// Open Weather Map date format: "2019-04-25 17:00:00"
+			// Change it to UTC date string: "2019-04-25T17:00:00Z"			
+			const dateArray = openWeatherMapDate.split(" ");
+			const UTCDateString = String.prototype.concat(dateArray[0], "T", dateArray[1], "Z");
+			return UTCDateString;
+		}
+		
+		function getAmPmHours(hourIn24h) {
+			/* return an object in the form of:
+				{
+					"hour": 10,
+					"tt": "pm"
+				}
+			*/
+			
+			if (!((hourIn24h >= 0) && (hourIn24h <= 23) && (Math.floor(hourIn24h) === hourIn24h))) {
+				throw new RangeError("hourIn24h must be an integer in [0, 23]");
+			}
+			
+			const am = "am", pm = "pm";
+			
+			const result = new (function () {
+				this.hour = 1;
+				this.tt = am;
+			})();
+				
+			if ((hourIn24h >= 0) && (hourIn24h <= 11)) {
+				if (hourIn24h === 0) {
+					result.hour = 12;
+				}
+				else {
+					result.hour = hourIn24h;
+				}
+				result.tt = am;
+			}
+			else {
+				if (hourIn24h === 12) {
+					result.hour = 12;
+				}
+				else {
+					result.hour = hourIn24h - 12;
+				}
+				result.tt = pm;
+			}
+			
+			return Object.freeze(result);
+		}
+		
+		Object.defineProperty(this, "displayTime", {
+			value: function (openWeatherMapDate) {
+				const UTCDateString = getUTCDateString(openWeatherMapDate);
+				const date = new Date(UTCDateString);
+				
+				const hourObj = getAmPmHours(date.getHours());
+				const hour = hourObj.hour;
+				const tt = hourObj.tt;
+				
+				const minute = date.getMinutes();
+				
+				if (minute < 10) {
+					return String.prototype.concat(hour, ":0", minute, " ", tt);
+				}
+				else {
+					return String.prototype.concat(hour, ":", minute, " ", tt);
+				}
+				
+			}
+		});
+		
+	}
+)();
+
 
 console.log(requestMaker);
 requestMaker.search("Davis,US");
